904e3b494c8fbf7be2e7ea0f9230a31f8f438fce
muh ikhsan <thisisli96@yahoo.com>
2019-12-06 05:11:47 +0700

    video sudah tampil di xml menggunakan Videoview

--- app/src/main/java/com/example/mrpeter1/videodemoudemy/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/videodemoudemy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mrpeter1.videodemoudemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        videoView.start();
     }
 }
